e8ff09a355b0ebea7e20a9ef3830e2d5075e8a94
Colby Albers <99503345+colby-albers@users.noreply.github.com>
2022-10-02 09:25:31 -0400

    Add files via upload

---
+++ for_loop_basic1.py
@@ -0,0 +1,25 @@
+for i in range (0, 151):
+    print(i)
+
+
+for i in range (5, 10001, 5):
+    print (i)
+
+
+for i in range (0, 101):
+    if i % 10==0:
+        print("Coding Dojo")
+    elif i % 5==0:
+        print("Coding")
+    elif i % 5 & 10==0:
+        print(i)
+
+sum = 0
+for i in range(1,500001,2):
+    sum += i
+    print(sum)
+
+for i in range (2018, 0, -4):
+    print(i)
+
+
